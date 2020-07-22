15af11ffa5d9b5bb543a9fbafc6bae1430295bfc
melonpan5 <hoshi2771@gmail.com>
2020-07-22 21:57:09 +0900

    [Update]メイラー設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "pf-01_#{Rails.env}"
 
   config.action_mailer.perform_caching = true
-
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            ENV.fetch("YOUR_EMAIL_ADDRESS"),
+    password:             ENV.fetch("YOUR_EMAIL_PASSWORD"),
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
